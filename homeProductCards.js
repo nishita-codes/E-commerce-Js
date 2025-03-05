0eae946a69266f2cb1aab2943b28ccd9ea5dbf51
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-05 13:48:35 +0530

    added files

---
+++ homeProductCards.js
@@ -0,0 +1,15 @@
+const productContainer = document.querySelector('#productContainer')
+
+const productTemplate = document.querySelector('#productTemplate')
+export const showProductContainer = (products) =>{
+if(!products){
+    return false;
+}
+products.forEach((curProd) =>{
+    const { brand , category , description , id , image , name , price, stock } = curProd;
+   
+
+    // importNode(): This  is method of the  document object. it is used to import a node  from another document or  from a <template>  elemtent.
+    const productClone =  document.
+});
+};
