a10b5258c15f956549e1524dde3d3a1661ec491f
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-19 20:30:13 +0530

    added files

--- src/homeProductCards.js
+++ src/homeProductCards.js
@@ -1,6 +1,5 @@
-const productContainer = document.querySelector('#productContainer')
-
-const productTemplate = document.querySelector('#productTemplate')
+const productContainer = document.querySelector("#productContainer");
+const productTemplate = document.querySelector("#productTemplate");
 export const showProductContainer = (products) =>{
 if(!products){
     return false;
@@ -10,7 +9,7 @@ products.forEach((curProd) =>{
    
 
     // importNode(): This  is method of the  document object. it is used to import a node  from another document or  from a <template>  elemtent.
-    const productClone =  document.importNode(productTemplate.content,true);
+    const productClone =  document.importNode(productTemplate.content , true);
 
     productClone.querySelector('.productName').textContent = name;
     productContainer.append(productClone);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './style.css';
 
 import products from "../api/products.json";
-import { showProductContainer } from '../homeProductCards';
-console.log(products);
+import { showProductContainer } from "./homeProductCards";
+
 
 // define the function name `showProductContainer` that takes an array of products as input
 showProductContainer(products);
